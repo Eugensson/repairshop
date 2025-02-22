6bf9b59e7ce42d49ce3493efeed9c8395f50b118
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-02-22 11:44:53 +0200

    Created Customer and Ticket forms

---
+++ src/app/(rs)/customers/form/page.tsx
@@ -0,0 +1,38 @@
+import { BackButton } from "@/components/BackButton";
+import CustomerForm from "@/app/(rs)/customers/form/CustomerForm";
+
+import { getCustomer } from "@/lib/queries/getCustomer";
+
+export default async function CustomerFormPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  try {
+    const { customerId } = await searchParams;
+
+    if (customerId) {
+      const customer = await getCustomer(parseInt(customerId));
+
+      if (!customer) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">
+              Customer ID #{customerId} not found
+            </h2>
+            <BackButton title="Go Back" variant="default" />
+          </>
+        );
+      }
+
+      return <CustomerForm customer={customer} />;
+    } else {
+      return <CustomerForm />;
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+      throw e;
+    }
+  }
+}
